343288c64157012471d493125c864adcf7a3de75
dain <57316771+DainPark-web@users.noreply.github.com>
2024-04-15 18:37:46 +0200

    update

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,14 +5,19 @@ import * as davizly from "../src/index.js";
 should();
 
 describe("App index test!", function(){
-    describe("mochaTest", function(){
-        it("mochaTest should return hello dain✅", function(){
+    describe("shape", function(){
+        it("it should return the value of positionX", function(){
             // assert.equal(davizly.mochaTest(), "hello dain")
-            davizly.mochaTest().should.equal("hello dain")
+            const circle = davizly.Circle({pos: {x: 1, y: 1}})
+            const pos = circle.pos.x
+            pos.should.equal(1)
         })
-        it("should be a string type✅", function(){
+        it("it should return object", function(){
             // assert.equal(davizly.mochaTest(), "hello dain")
-            davizly.mochaTest().should.be.a("string")
+            const circle = davizly.Circle({pos: {x: 1, y: 1}})
+            const pos = circle
+            pos.should.be.a("object")
         })
+       
     })
 })
