43f83b07af41271df597bf92b03e0e00b63a1d9f
dain <57316771+DainPark-web@users.noreply.github.com>
2024-04-15 09:39:04 +0200

    update

---
+++ dist/bundle.js
@@ -0,0 +1,14 @@
+const testC = () => {
+    console.log("dddd");
+};
+
+testC();
+const select = (path) => {
+    const selected = document.querySelector(path);
+    
+    return selected;
+};
+
+console.log("dain");
+
+export { select };
