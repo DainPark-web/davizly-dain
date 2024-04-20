2db0bdadd7f85110cdd124b093d3bda8ff93ef87
dain <57316771+DainPark-web@users.noreply.github.com>
2024-04-20 21:27:45 +0200

    unit testing

--- test/shape.spec.js
+++ test/shape.spec.js
@@ -8,7 +8,9 @@ describe("DOM Manipulation!", function () {
   let document;
   beforeEach(function () {
     // fake dom
-    const jsdom = new JSDOM(`<!DOCTYPE html><html><body></body></html>`);
+    const jsdom = new JSDOM(`<!DOCTYPE html><html><body></body></html>`, {
+        url: "http://localhost"
+    });
     document = jsdom.window.document;
   });
   describe("BasicShape", function () {
@@ -18,7 +20,17 @@ describe("DOM Manipulation!", function () {
           this.svgns = "http://www.w3.org/2000/svg";
           this.shape = document.createElementNS(this.svgns, shapeName);
         }
+      }
+      const bH = new BasicShape("circle");
+      bH.should.be.a("object");
+    });
 
+    it("it should return 100, SVGElement, children length 1", function () {
+      class BasicShape {
+        constructor(shapeName) {
+          this.svgns = "http://www.w3.org/2000/svg";
+          this.shape = document.createElementNS(this.svgns, shapeName);
+        }
         appendTo(parent) {
           parent.append(this.shape);
           return this;
@@ -31,13 +43,18 @@ describe("DOM Manipulation!", function () {
           return this.shape;
         }
       }
-      const bH = new BasicShape("circle")
+      const container = document.querySelector("body")
+      const bH = new BasicShape("circle");
+      bH.appendTo(container)
       bH.attrTo("cx", 100);
       const xPos = bH.node().getAttribute("cx");
+      const circleNode = bH.node()
+      const len = container.children.length
 
 
-      bH.should.be.a("object");
-      xPos.should.equal("100")
+      xPos.should.equal("100");
+      circleNode.should.be.a("SVGElement");
+      len.should.equal(1);
     });
   });
 });
