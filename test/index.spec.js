9957dd7e7ad6bf2a16043dc7529fc41fb31e414d
dain <57316771+DainPark-web@users.noreply.github.com>
2024-04-20 20:50:09 +0200

    update

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,23 +1,43 @@
 // import assert from "assert";
-import {should} from "chai";
-import * as davizly from "../src/index.js";
+import { should } from "chai";
+import { JSDOM } from "jsdom";
 
 should();
 
-describe("App index test!", function(){
-    describe("shape", function(){
-        it("it should return the value of positionX", function(){
-            // assert.equal(davizly.mochaTest(), "hello dain")
-            const circle = davizly.Circle({pos: {x: 1, y: 1}})
-            const pos = circle.pos.x
-            pos.should.equal(1)
-        })
-        it("it should return object", function(){
-            // assert.equal(davizly.mochaTest(), "hello dain")
-            const circle = davizly.Circle({pos: {x: 1, y: 1}})
-            const pos = circle
-            pos.should.be.a("object")
-        })
-       
-    })
-})
+describe("DOM Manipulation!", function () {
+  let document;
+  beforeEach(function () {
+    // fake dom
+    const jsdom = new JSDOM(`<!DOCTYPE html><html><body></body></html>`);
+    document = jsdom.window.document;
+  });
+  describe("BasicShape", function () {
+    it("it should return object", function () {
+      class BasicShape {
+        constructor(shapeName) {
+          this.svgns = "http://www.w3.org/2000/svg";
+          this.shape = document.createElementNS(this.svgns, shapeName);
+        }
+
+        appendTo(parent) {
+          parent.append(this.shape);
+          return this;
+        }
+        attrTo(att, value) {
+          this.shape.setAttributeNS(null, att, value);
+          return this;
+        }
+        node() {
+          return this.shape;
+        }
+      }
+      const bH = new BasicShape("circle")
+      bH.attrTo("cx", 100);
+      const xPos = bH.node().getAttribute("cx");
+
+
+      bH.should.be.a("object");
+      xPos.should.equal("100")
+    });
+  });
+});
