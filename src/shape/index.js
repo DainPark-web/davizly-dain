47013b6f720131c3790754e854011b5cda2e8393
dain <57316771+DainPark-web@users.noreply.github.com>
2024-04-20 10:42:00 +0200

    update

--- src/shape/index.js
+++ src/shape/index.js
@@ -1,8 +1,8 @@
 export * as Circle from "./circle.js"
-// export {default as Rect} from "./rect.js"
-// export {default as G} from "./g.js"
-// export {default as Line} from "./line.js"
-// export {default as SVG} from "./svg.js"
+export {default as Rect} from "./rect.js"
+export {default as G} from "./g.js"
+export {default as Line} from "./line.js"
+export {default as SVG} from "./svg.js"
 
 
 
